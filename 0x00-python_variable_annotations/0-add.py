548b903c0f0c7eb285b87fd18f6a71c382671e1f
Iganza-roy <iganzaroy55@gmail.com>
2024-12-16 14:55:48 +0300

    Add function to add two floating-point numbers

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# This module contains a function that adds two floating-point numbers.
+
+def add(a: float, b: float) -> float:
+    """
+    This function adds two floating-point numbers.
+    """
+    return a + b 
